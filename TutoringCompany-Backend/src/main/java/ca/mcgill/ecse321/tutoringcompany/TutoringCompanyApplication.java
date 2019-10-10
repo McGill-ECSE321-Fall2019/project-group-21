3d97b704ef974ac9fc74f8cd7972e1d99729250d
eliastam <51382208+eliastam@users.noreply.github.com>
2019-10-09 20:52:50 -0400

    Initial commit of the backend application

---
+++ TutoringCompany-Backend/src/main/java/ca/mcgill/ecse321/tutoringcompany/TutoringCompanyApplication.java
@@ -0,0 +1,22 @@
+package ca.mcgill.ecse321.tutoringcompany;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+@SpringBootApplication
+public class TutoringCompanyApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(TutoringCompanyApplication.class, args);
+  }
+
+  @RequestMapping("/")
+  public String greeting(){
+    return "Hello world!";
+  }
+
+}
+
