16beb5c5eda993a19e9c24464b5a68a79a31663a
eliastam <51382208+eliastam@users.noreply.github.com>
2019-10-31 18:35:26 -0400

    Revert "session test fixed"
    This reverts commit 27c2a08207fc3d83ec03ce3d86f75052f1748cd4.
    
    i
    i

--- TutoringCompany-Backend/src/test/java/ca/mcgill/ecse321/tutoringcompany/service/TestRoom.java
+++ TutoringCompany-Backend/src/test/java/ca/mcgill/ecse321/tutoringcompany/service/TestRoom.java
@@ -13,7 +13,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import ca.mcgill.ecse321.tutoringcompany.dao.RoomRepository;
-import ca.mcgill.ecse321.tutoringcompany.dao.SessionRepository;
 import ca.mcgill.ecse321.tutoringcompany.dao.StudentRepository;
 import ca.mcgill.ecse321.tutoringcompany.model.Room;
 import ca.mcgill.ecse321.tutoringcompany.model.RoomType;
@@ -34,14 +33,11 @@ public class TestRoom {
 
 	@Autowired
 	private RoomRepository roomRepository;
-	@Autowired
-	private SessionRepository sessionRepository;
 	
-	@Before
-	public void clearDatabase() {
-		sessionRepository.deleteAll();
-		roomRepository.deleteAll();
-	}
+//	@Before
+//	public void clearDatabase() {
+//		roomRepository.deleteAll();
+//	}
 	
 	/**
 	 * Create a room
