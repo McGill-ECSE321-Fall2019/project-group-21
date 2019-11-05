e9e55547fd65fa606e79e3275692af5aeb72e011
GeorgeKandalaft <george.kandalaft@mail.mcgill.ca>
2019-11-05 00:16:57 -0500

    first page

---
+++ tutoringcompany-frontend/src/components/TutoringCompany.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="MainPage">
+    <h1>Welcome to Quality over Quantity Tutoring Company</h1>
+    
+    <table>
+        <h2>Login</h2>
+      <tr>
+          <td>Email:</td>
+          <td><input type="text" placeholder="email"></td>
+      </tr>
+      <tr>
+          <td>Password:
+
+          </td>
+          <td>
+              <input type="password" placeholder="password">
+          </td>
+          <td>
+              <button>Login</button>
+          </td>
+      </tr>
+    </table>
+    <p>
+      <span style="color:red">Error: Message text comes here</span>
+    </p>
+  </div>
+</template>
+<script>
+</script>
+<style>
+  #MainPage {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    color: #172431;
+    font-size: 25px;
+    background: #0661b6;
+  }
+</style>
--- tutoringcompany-frontend/src/router/index.js
+++ tutoringcompany-frontend/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Hello from '@/components/Hello'
+import TutoringCompany from '@/components/TutoringCompany'
 
 Vue.use(Router)
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'Hello',
       component: Hello
+    },
+    {
+      path: '/app',
+      name: 'TutoringCompany',
+      component: TutoringCompany
     }
   ]
 })
