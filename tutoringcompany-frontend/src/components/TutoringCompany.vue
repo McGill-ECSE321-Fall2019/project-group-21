e6a53c5b29ffcd1fabb78556f902c8d8fe527f38
GeorgeKandalaft <george.kandalaft@mail.mcgill.ca>
2019-11-11 22:10:54 -0500

    :beer:

--- tutoringcompany-frontend/src/components/TutoringCompany.vue
+++ tutoringcompany-frontend/src/components/TutoringCompany.vue
@@ -1,60 +1,61 @@
-<template>
-  <div id="MainPage">
+ <template>
+   <div id="WelcomePage">
     <h1>Welcome to Quality over Quantity Tutoring Company</h1>
-    <div id="Options"> 
-      <tr>
-      <td>
-      <a href="http://127.0.0.1:8087/#/" class="button">Our Tutors</a>
-      </td>
-      <td> &nbsp;&nbsp;&nbsp;&nbsp;
-        </td>
-      <td>
-      <a href="http://127.0.0.1:8087/#/" class="button">Our Course</a>
-      </td>
-      </tr>
-    </div>    
-    <table>
-        <h2>Login</h2>
-      <tr>
-          <td>Email:</td>
-          <td><input type="text" v-model="email" placeholder="email"></td>
-      </tr>
-      <tr>
-          <td>Password:
-          </td>
-          <td>
-              <input type="password" v-model="password" placeholder="password">
-          </td>
-      </tr>
-          <td>
-              <a href="http://127.0.0.1:8087/#/login" class="button">Log in as a manager</a>
-          </td>
-      
-    </table>
+     <div id= "about">
+        <h2>About us: </h2>
+        <p> Who We Are?
+            <b> Quality Over Quantity <a href="http://google.com">Tutors</a></b> 
+            are an elite team of certified teachers who offer the best tutoring services available.
+            Your child will be getting the instruction they need with our company. We are qualified to teach, 
+        but we believe in the power of one-on-one tutoring. Our goal is to make sure your son or daughter has the tools and confidence to score straight A’s—while enjoying the process! 
+          </p>
+          </div>
+          <div id= "Services">
+         <p>Our Tutors:</p>
+         <p>Our Courses:</p>
+         <p>Our Offerings:</p>
+         <p>Login</p>
+         <p>Sign up</p>
+</div>
     <p>
-      <span style="color:red">Error: Message text comes here</span>
+       <span v-if="errorSignUp" style="color:red">Error: {{errorSignUp}} </span>
+       <span v-if="response" style="color:green">Success: {{response}} </span>
     </p>
-  </div>
-  
-</template>
+   </div> 
+ </template>
 <script>
 </script>
 <style>
 
-  #MainPage {
+  #WelcomePage {
     font-family: 'Avenir', Helvetica, Arial, sans-serif;
     color: #172431;
     font-size: 25px;
     background: #a7c5e0;
     text-align: inherit
   }
-
-  #Options{
-    display: grid;
-    font-family: 'Avenir', Helvetica, Arial, sans-serif;
-    color: #1a7ee2;
-    font-size: 40px;
-    background: #a7c5e0;
-    text-align: inherit
+  h2{
+     text-align: left
   }
+#about {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    color: #172431;
+    font-size: 22px;
+    background: #a7c5e0;
+    text-align: center;
+    display: block;
+    grid-template-columns: 100px  600px
+  }
+
+  #Services{
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    color: #172431;
+    font-size: 22px;
+    background: #a7c5e0;
+    text-align: end;
+    display: grid;
+    grid-area: auto;
+    grid-template-columns: 200px 200px 200px 200px 200px
+  }
+
 </style>
