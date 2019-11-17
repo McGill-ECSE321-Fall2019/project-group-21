fdf9bd8e8d1d9c3ef69639242a7171dfa8959965
GeorgeKandalaft <george.kandalaft@mail.mcgill.ca>
2019-11-16 19:34:01 -0500

    TUTORS VERIFICATION

--- tutoringcompany-frontend/src/components/tutorpg.js
+++ tutoringcompany-frontend/src/components/tutorpg.js
@@ -16,32 +16,27 @@ var AXIOS = axios.create({
         tutors: [],
         newPerson: '',
         errorPerson: '',
-        email: '',
         tutor: '',
       }
     },
 created: function () {
-    // Initializing tutors from backend
-AXIOS.get(`/Manager/get/allTutors`)
-.then(response => {
-  // JSON responses are automatically parsed.
-  this.tutors = response.data
-})
-.catch(e => {
-  this.errorPerson = e;
-});
-},
-methods: {
-  getTutor: function (email) {
+   var email = window.location.href.substring(34)
       AXIOS.post(`/Manager/get/Tutor/`+ "?email=" + email , {}, {})
       .then(response => {
         // JSON responses are automatically parsed.
         this.tutor = response.data
-        window.location.href = "/#/tutorPage/"+ email
-      })
+      });
+    },
+    methods: {
+        verifyTutor: function(email){
+            AXIOS.post(`/Manager/VerifyTutor`+ "?email=" + email , {}, {})
+            .then(response => {
+                // JSON responses are automatically parsed.
+                this.tutor = response.data
+              })
+        }
+
     }
-  }
-}
 
-
+    }
   
