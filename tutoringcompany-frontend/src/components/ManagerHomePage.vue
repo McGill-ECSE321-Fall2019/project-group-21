1fe7db5f5c2768ba88cc45bd677ea79c43b1eb40
eliastam <51382208+eliastam@users.noreply.github.com>
2019-11-16 22:05:53 -0500

    conflict solved

--- tutoringcompany-frontend/src/components/ManagerHomePage.vue
+++ tutoringcompany-frontend/src/components/ManagerHomePage.vue
@@ -1,96 +1,96 @@
 <!DOCTYPE html>
 <template>
-  <html lang="en">
-
+<html lang="en">
   <head>
     <title>Bootstrap Example</title>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
   </head>
-
   <body>
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <router-link to = '/ManagerHomePage' class="navbar-brand" href="#">Quality Academy</router-link>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light static-top">
+    <!-- <div class="container"> -->
+    <a class="navbar-brand" href="#">
+      <img
+        alt="Brand"
+        src="../assets/logo.png"
+        class="image-responsive"
+        style="max-width: 10%;overflow: visible;"
+      />Quality over Quantity
+    </a>
+    <button
+      class="navbar-toggler"
+      type="button"
+      data-toggle="collapse"
+      data-target="#navbarResponsive"
+      aria-controls="navbarResponsive"
+      aria-expanded="false"
+      aria-label="Toggle navigation"
+    >
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarResponsive">
+      <ul class="navbar-nav ml-auto">
+        <li class="nav-item active">
+          <router-link to="/ManagerHomePage" class="nav-link">
+            Manager Home
+            <span class="sr-only">(current)</span>
+          </router-link>
+        </li>
+        <li class="nav-item">
+          <router-link to="/TutorsM" class="nav-link">Tutors</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link to="/StudentsM" class="nav-link">Students</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link to="/CoursesM" class="nav-link">Courses</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link to="/RoomsM" class="nav-link">Rooms</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link to="/SessionsM" class="nav-link">Sessions</router-link>
+        </li>
+      </ul>
+      <ul class="navbar-nav ml-auto">
+        <li class="nav-item">
+          <router-link to="/Manager" class="nav-link">Profile</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link to="/Logout" class="nav-link">Logout</router-link>
+        </li>
+      </ul>
+    </div>
+    <!-- </div> uncomment to make the nav bar more narrow -->
+  </nav>
 
-  <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
-    <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
-      <li class="nav-item">
-        <router-link to="/TutorsM" class="nav-link" id="tutors">Tutors  </router-link>
-      </li>
+    <h1 id="greeting">Hi you have Secsessfully logged in</h1>
+    <img src="../assets/quality.jpg" alt="logo" id="logo" />
 
-      <li class="nav-item">
-        <router-link to = '/TutorsM' class="nav-link"> Rooms </router-link>
-      </li>
-      <li class="nav-item">
-        <router-link to = '/' class="nav-link" >Courses</router-link>
-        
-      </li>
-      <li class="nav-item">
-        <router-link to = '/' class="nav-link" >Students</router-link>
-        
-      </li>
-      <li class="nav-item">
-        <router-link to = '/' class="nav-link" >Sessions</router-link>
-        
-      </li>
-      <li class="nav-item">
-        <router-link to = '/' class="nav-link" >Managemet Settings</router-link>
-        
-      </li>
-       <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Account
-        </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-          <a class="dropdown-item" href="#">Logout</a>
-          <a class="dropdown-item" href="#">Edit Profile</a>
-        
-        </div>
-      </li>
-    </ul>
-    
-  </div>
-</nav>
-
-<h1 id="greeting">Hi you have Secsessfully logged in</h1>
-<img src="../assets/quality.jpg" alt="logo" id="logo">
-
-</body>
-
+    </body>
 </html>
 </template>
 
 <style>
-#greeting{
+/* #greeting{
 text-align: center;
 }
 #logo{
  margin-left: 30%;
 }
-
 #tutors{
-/* 
-  --to be implemented */
+ 
+  //--to be implemented
   }
-
-
 .nav-link{
   font-size: 20px;
 margin-left: 20px;
-
-
   
  
-}
-
+} */
 </style>
 
 <script>
-  import jsPDF from 'jspdf';
-  import axios from 'axios'
-  
+// import jsPDF from "jspdf";
+// import axios from "axios";
 </script>
