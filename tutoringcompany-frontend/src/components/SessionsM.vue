19683df0a4fe74a6b1f4ad8ec6a784366fb80ff5
GeorgeKandalaft <george.kandalaft@mail.mcgill.ca>
2019-11-18 02:27:43 -0500

    FIREEEEEEE

--- tutoringcompany-frontend/src/components/SessionsM.vue
+++ tutoringcompany-frontend/src/components/SessionsM.vue
@@ -5,87 +5,76 @@
     <title>Manage Sessions</title>
   </head>
   <body>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light static-top">
-      <!-- <div class="container"> -->
-      <router-link
-        to="/HomePage"
-        class="navbar-brand mr-0"
-        style="max-width:calc((1em + 1vw) * 7 + .5rem + 1vw + 30px);font-size:calc(1em + 1vw);"
-      >
-        <img
-          alt="Brand"
-          src="../assets/quality.png"
-          class="image-responsive mr-2"
-          style="max-width:calc(30px + 1vw);overflow: visible;"
-        />Quality Academy
-      </router-link>
-      <button
-        class="navbar-toggler"
-        type="button"
-        data-toggle="collapse"
-        data-target="#navbarResponsive"
-        aria-controls="navbarResponsive"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <span class="navbar-toggler-icon"></span>
-      </button>
-
-      <div class="collapse navbar-collapse" id="navbarResponsive">
-        <!-- <div class="float-right"> -->
-        <ul class="navbar-nav ml-auto">
-          <li class="nav-item">
-            <router-link to="/ManagerHomePage" class="nav-link">Manager Home</router-link>
-          </li>
-          <li class="nav-item">
-            <router-link to="/TutorsM" class="nav-link">Tutors</router-link>
-          </li>
-          <li class="nav-item">
-            <router-link to="/StudentsM" class="nav-link">Students</router-link>
-          </li>
-          <li class="nav-item">
-            <router-link to="/CoursesM" class="nav-link">Courses</router-link>
-          </li>
-          <li class="nav-item">
-            <router-link to="/RoomsM" class="nav-link">Rooms</router-link>
-          </li>
-          <li class="nav-item active">
-            <router-link to="/SessionsM" class="nav-link">
-              Sessions
-              <span class="sr-only">(current)</span>
-            </router-link>
-          </li>
-        </ul>
-        <!-- </div> uncomment for main links left justified-->
-        <ul class="navbar-nav ml-auto">
-          <li class="nav-item dropdown">
-            <a
-              class="nav-link"
-              href="#"
-              id="navbarDropdown"
-              data-toggle="dropdown"
-              aria-haspopup="true"
-              aria-expanded="false"
-            >
+  <nav class="navbar navbar-expand-lg navbar-light bg-light static-top">
+    <!-- <div class="container"> -->
+    <router-link to="/HomePage" class="navbar-brand mr-0" style="max-width:calc((1em + 1vw) * 7 + .5rem + 1vw + 30px);font-size:calc(1em + 1vw);">
+      <img
+        alt="Brand"
+        src="../assets/quality.png"
+        class="image-responsive mr-2"
+        style="max-width:calc(30px + 1vw);overflow: visible;"
+      />Quality Academy
+    </router-link>
+    <button
+      class="navbar-toggler"
+      type="button"
+      data-toggle="collapse"
+      data-target="#navbarResponsive"
+      aria-controls="navbarResponsive"
+      aria-expanded="false"
+      aria-label="Toggle navigation"
+    >
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    
+    <div class="collapse navbar-collapse" id="navbarResponsive">
+      <!-- <div class="float-right"> -->
+      <ul class="navbar-nav ml-auto">
+        <li class="nav-item">
+          <router-link to="/ManagerHomePage" class="nav-link">Manager Home</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link to="/TutorsM" class="nav-link">
+            Tutors
+          </router-link>
+        </li>
+        <li class="nav-item">
+          <router-link to="/StudentsM" class="nav-link">Students</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link to="/CoursesM" class="nav-link">Courses</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link to="/RoomsM" class="nav-link">Rooms</router-link>
+        </li>
+        <li class="nav-item active">
+          <router-link to="/SessionsM" class="nav-link">Sessions
+          <span class="sr-only">(current)</span>
+          </router-link>
+        </li>
+      </ul>
+      <!-- </div> uncomment for main links left justified--> 
+      <ul class="navbar-nav ml-auto">
+        <li class="nav-item dropdown">
+            <a class="nav-link" href="#" id="navbarDropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
               <i class="fa fa-user"></i>
               <i class="fa fa-caret-down"></i>
             </a>
             <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-              <a class="dropdown-item" href="#">Profile</a>
-              <a @click="ManagerLogout()" class="dropdown-item">Logout</a>
+                <a class="dropdown-item" href="#">Profile</a>
+                <a @click="ManagerLogout()" class="dropdown-item" >Logout</a>
             </div>
-          </li>
-        </ul>
-      </div>
-      <!-- </div> uncomment to make the nav bar more narrow -->
-    </nav>
-    <div class="jumbotron jumbotron-fluid">
-      <div class="container">
-        <h1 class="display-4">Sessions</h1>
-        <p
-          class="lead"
-        >Gallery of all sessions, view all sessions or pending sessions, and assign a room to any session by choosing from the dropdown.</p>
-      </div>
+        </li>
+      </ul>
+    </div>
+    <!-- </div> uncomment to make the nav bar more narrow -->
+  </nav>
+  <div class="jumbotron jumbotron-fluid">
+    <div class="container">
+      <h1 class="display-4">Sessions</h1>
+      <p
+        class="lead"
+      >Gallery of all sessions, view all sessions or pending sessions, and assign a room to any session by choosing from the dropdown.</p>
     </div>
   </div>
   <div class="container-fluid">
@@ -100,7 +89,6 @@
               class="card-text"
             ></p>
           </div>
-
           <ul class="list-group list-group-flush">
             <li class="list-group-item">Starts: {{session.start_time}}</li>
             <li class="list-group-item">Ends: {{session.end_time}}</li>
@@ -110,8 +98,9 @@
                <li class="list-group-item">Course ID: {{session.offeringDto.course.course_id}}</li>
           </ul>
         </div>
-      </div>
+      </div> 
     </div>
+  </div>
   </body>
 </html>
 </template>
