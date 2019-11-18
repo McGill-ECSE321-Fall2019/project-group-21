0d760dc54d932d2c480cf6108624e986b009814a
Louca Dufault <louca.dufault@gmail.com>
2019-11-17 22:32:24 -0500

    contact page finished

---
+++ tutoringcompany-frontend/src/components/AboutContact.vue
@@ -0,0 +1,177 @@
+<!DOCTYPE html>
+<template>
+<html lang="en">
+  <head>
+    <title>Manage Rooms</title>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+  </head>
+  <body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light static-top">
+      <!-- <div class="container"> -->
+      <router-link
+        to="/HomePage"
+        class="navbar-brand mr-0"
+        style="max-width:calc((1em + 1vw) * 7 + .5rem + 1vw + 30px);font-size:calc(1em + 1vw);"
+      >
+        <img
+          alt="Brand"
+          src="../assets/quality.png"
+          class="image-responsive mr-2"
+          style="max-width:calc(30px + 1vw);overflow: visible;"
+        />Quality Academy
+      </router-link>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarResponsive"
+        aria-controls="navbarResponsive"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarResponsive">
+        <!-- <div class="float-right"> -->
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item">
+            <router-link to="/ManagerHomePage" class="nav-link">Home</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link to="/TutorsM" class="nav-link">Tutors</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link to="/StudentsM" class="nav-link">Students</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link to="/CoursesM" class="nav-link">Courses</router-link>
+          </li>
+          <li class="nav-ite">
+            <router-link to="/RoomsM" class="nav-link">Rooms</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link to="/SessionsM" class="nav-link">Sessions</router-link>
+          </li>
+          <li class="nav-ite active">
+            <router-link to="/AboutContact" class="nav-link">
+              About &amp; Contact
+              <span class="sr-only">(current)</span>
+            </router-link>
+          </li>
+        </ul>
+        <!-- </div> uncomment for main links left justified-->
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item dropdown">
+            <a
+              class="nav-link"
+              href="#"
+              id="navbarDropdown"
+              data-toggle="dropdown"
+              aria-haspopup="true"
+              aria-expanded="false"
+            >
+              Get started
+              <i class="fa fa-caret-down"></i>
+            </a>
+            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+              <a class="dropdown-item" href="#">Book a session</a>
+              <a class="dropdown-item" href="#">Work with us</a>
+            </div>
+          </li>
+        </ul>
+      </div>
+      <!-- </div> uncomment to make the nav bar more narrow -->
+    </nav>
+    <div class="jumbotron jumbotron-fluid">
+      <div class="container">
+        <h1 class="display-4">Welcome to Quality Academy</h1>
+        <p class="lead">
+          The Quality Over Quantity Tutoring Company aims to provide students with the opportunity to connect with expert tutors in their field.
+          As a new startup company (founded in 2019), our simple but efficient website and mobile interfaces allow easy access to our services.
+          Students can find tutors at all levels of education (elementary school, high school, cegep and university) covering all subjects and courses.
+          We strive to give students the education they deserve, and offer exciting opportunities for tutors to help their peers.
+        </p>
+      </div>
+    </div>
+    <div class="container">
+      <div class="card text-center mx-auto mb-4 col-12">
+        <div class="card-body">
+          <h2 class="card-title">Contact Us</h2>
+          <p
+            class="card-text"
+          >Please let us know about any questions or suggestions you may have about our services or website.</p>
+          <p><i class="fa fa-phone mr-2"></i>+1 (438) 828 2332</p>
+          <p><i class="fa fa-envelope mr-2"></i>support@qualityacademy.com</p>
+          <p><i class="fa fa-map-marker mr-2"></i>3630 Rue University, Montréal, QC H3A 2B3</p>
+          <button class="btn btn-primary mx-1" type="button">
+            <a
+              href="https://www.facebook.com/QualityAcademy/"
+              style="color:inherit;text-decoration:inherit;"
+            >
+              Facebook
+              <i class="fa fa-facebook ml-3"></i>
+            </a>
+          </button>
+          <button class="btn btn-info mx-1" type="button">
+            <a
+              href="https://twitter.com/QualityAcademy/"
+              style="color:inherit;text-decoration:inherit;"
+            >
+              Twitter
+              <i class="fa fa-twitter ml-3"></i>
+            </a>
+          </button>
+          <button class="btn btn-danger mx-1" type="button">
+            <a
+              href="mailto:customersupport@qualityacademy.com"
+              style="color:inherit;text-decoration:inherit;"
+            >
+              Email
+              <i class="fa fa-envelope ml-3"></i>
+            </a>
+          </button>
+          <img class="mt-4" src="../assets/map.png" alt="map" style="max-width:100%;">
+        </div>
+      </div>
+    </div>
+    <div class="container-fluid">
+      <div class="row">
+        <div v-for="room in rooms" class="col-12 col-md-6 col-lg-4 col-xl-3 mb-4">
+          <div class="card mx-auto text-center">
+            <div class="card-body">
+              <h5 class="card-title">
+                <router-link
+                  v-bind:to="'/' + tutor.email"
+                >{{ tutor.first_name }} {{tutor.last_name}}</router-link>
+              </h5>
+              <h6 class="card-subtitle mb-2 text-muted">Tutor</h6>
+              <p
+                class="card-text"
+              >Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
+            <ul class="list-group list-group-flush">
+              <li class="list-group-item">Email: {{tutor.email}}</li>
+              <li class="list-group-item">Phone: {{tutor.phone_number}}</li>
+              <li class="list-group-item">Verified: {{tutor.verified}}</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
+</template>
+
+
+
+<style>
+.card {
+  max-height: 100rem;
+}
+#btn {
+  margin-left: 80%;
+  margin-bottom: 10px;
+}
+</style>
---
+++ tutoringcompany-frontend/src/components/CoursesM.vue
@@ -0,0 +1,160 @@
+<!DOCTYPE html>
+<template>
+<html lang="en">
+  <head>
+    <title>Manage Courses</title>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+  </head>
+  <body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light static-top">
+      <!-- <div class="container"> -->
+      <router-link
+        to="/HomePage"
+        class="navbar-brand mr-0"
+        style="max-width:calc((1em + 1vw) * 7 + .5rem + 1vw + 30px);font-size:calc(1em + 1vw);"
+      >
+        <img
+          alt="Brand"
+          src="../assets/quality.png"
+          class="image-responsive mr-2"
+          style="max-width:calc(30px + 1vw);overflow: visible;"
+        />Quality Academy
+      </router-link>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarResponsive"
+        aria-controls="navbarResponsive"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarResponsive">
+        <!-- <div class="float-right"> -->
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item">
+            <router-link to="/ManagerHomePage" class="nav-link">Manager Home</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link to="/TutorsM" class="nav-link">Tutors</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link to="/StudentsM" class="nav-link">Students</router-link>
+          </li>
+          <li class="nav-item active">
+            <router-link to="/CoursesM" class="nav-link">
+              Courses
+              <span class="sr-only">(current)</span>
+            </router-link>
+          </li>
+          <li class="nav-ite">
+            <router-link to="/RoomsM" class="nav-link">Rooms</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link to="/SessionsM" class="nav-link">Sessions</router-link>
+          </li>
+        </ul>
+        <!-- </div> uncomment for main links left justified-->
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item dropdown">
+            <a
+              class="nav-link"
+              href="#"
+              id="navbarDropdown"
+              data-toggle="dropdown"
+              aria-haspopup="true"
+              aria-expanded="false"
+            >
+              <i class="fa fa-user"></i>
+              <i class="fa fa-caret-down"></i>
+            </a>
+            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+              <a class="dropdown-item" href="#">Profile</a>
+              <a class="dropdown-item" href="#">Logout</a>
+            </div>
+          </li>
+        </ul>
+      </div>
+      <!-- </div> uncomment to make the nav bar more narrow -->
+    </nav>
+    <div class="jumbotron jumbotron-fluid">
+      <div class="container">
+        <h1 class="display-4">Courses</h1>
+        <p
+          class="lead"
+        >Gallery of all course, add a course using the form below or delete any course.</p>
+      </div>
+    </div>
+    <div class="container">
+      <div class="card mx-auto mb-4 col-12">
+        <div class="card-body">
+          <h5 class="card-title">Add a course</h5>
+          <p class="card-text">Input the course name and course ID, and choose the subject.</p>
+
+          <form>
+            <div class="form-group">
+              <label for="exampleFormControlInput1">Course Name</label>
+              <input
+                type="text"
+                class="form-control"
+                placeholder="Introduction to Software Engineering"
+              />
+            </div>
+            <div class="form-group">
+              <label for="exampleFormControlInput1">Course ID</label>
+              <input type="text" class="form-control" placeholder="ECSE321" />
+            </div>
+            <div class="form-group">
+              <label for="exampleFormControlSelect1">Example select</label>
+              <select class="form-control" id="exampleFormControlSelect1">
+                <option v-for="subject in subjects">{{ subject }}</option>
+              </select>
+            </div>
+          </form>
+          <button type="submit" class="btn btn-primary float-right">Save course</button>
+        </div>
+      </div>
+    </div>
+    <div class="container-fluid">
+      <div class="row">
+        <div v-for="room in rooms" class="col-12 col-md-6 col-lg-4 col-xl-3 mb-4">
+          <div class="card mx-auto text-center">
+            <div class="card-body">
+              <h5 class="card-title">
+                <router-link
+                  v-bind:to="'/' + tutor.email"
+                >{{ tutor.first_name }} {{tutor.last_name}}</router-link>
+              </h5>
+              <h6 class="card-subtitle mb-2 text-muted">Tutor</h6>
+              <p
+                class="card-text"
+              >Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
+            <ul class="list-group list-group-flush">
+              <li class="list-group-item">Email: {{tutor.email}}</li>
+              <li class="list-group-item">Phone: {{tutor.phone_number}}</li>
+              <li class="list-group-item">Verified: {{tutor.verified}}</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
+</template>
+
+
+
+<style>
+.card {
+  max-height: 100rem;
+}
+#btn {
+  margin-left: 80%;
+  margin-bottom: 10px;
+}
+</style>
